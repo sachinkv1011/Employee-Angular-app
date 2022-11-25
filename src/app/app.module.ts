9ee06bda3072a87b9c0e7d18b2c053cf36b1f370
SACHIN K V <sachin.kv.1011@gmail.com>
2022-11-25 08:32:09 +0530

    employee angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,61 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { SearchEmployeeComponent } from './search-employee/search-employee.component';
+import { DeleteEmployeeComponent } from './delete-employee/delete-employee.component';
+import { EditEmployeeComponent } from './edit-employee/edit-employee.component';
+import { ViewEmployeeComponent } from './view-employee/view-employee.component';
+import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+
+const myRoutes:Routes = [
+  {
+    path:"",
+    component:AdminLoginComponent
+  },
+  {
+    path:"add",
+    component:AddEmployeeComponent
+  },
+  {
+    path:"search",
+    component:SearchEmployeeComponent
+  },
+  {
+    path:"edit",
+    component:EditEmployeeComponent
+  },
+  {
+    path:"delete",
+    component:DeleteEmployeeComponent
+  },
+  {
+    path:"view",
+    component:ViewEmployeeComponent
+  }
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AdminLoginComponent,
+    AddEmployeeComponent,
+    SearchEmployeeComponent,
+    DeleteEmployeeComponent,
+    EditEmployeeComponent,
+    ViewEmployeeComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(myRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
