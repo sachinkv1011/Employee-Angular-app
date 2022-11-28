effb96836677d90e7b7e107e9cf4b466e5f5acd0
SACHIN K V <sachin.kv.1011@gmail.com>
2022-11-28 12:49:34 +0530

    reading values

--- src/app/add-employee/add-employee.component.ts
+++ src/app/add-employee/add-employee.component.ts
@@ -6,7 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-employee.component.css']
 })
 export class AddEmployeeComponent {
- 
+  name=""
+  designation=""
+  salary=""
+  Company=""
+
+  readValues=()=>
+  {
+    let datas:any={
+      
+      "name":this.name,
+      "designation":this.designation,
+      "salary":this.salary,
+      "Company":this.Company,
+
+    }
+    console.log(datas)
+  }
 
 
 
